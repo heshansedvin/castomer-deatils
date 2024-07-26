c5c70819bd63cadb407a23d724c345e84e82ca7d
Heshan S Edvin <heshansankalpa2004@gmail.com>
2024-07-26 21:25:46 +0530

    1

---
+++ credentials.php
@@ -0,0 +1,4 @@
+<?php
+$name = "Vertex Regedit"; // the name of your application
+$OwnerId = "uMo5GzEQ1E"; // can be found in your account settings
+$SellerKey = "16813054a913cad969c933c39438def3"; // application's seller key. Found in seller settings (You must have seller plan)
